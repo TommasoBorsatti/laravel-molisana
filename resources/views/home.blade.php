c7c5c5ead3b93a39f012b8f8df9672f6d8c7f6ac
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-05-08 13:35:30 +0200

    Footer completato - Sistemato lo stile. Home Page finita.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,37 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Home</title>
+        <title>La Molisana - Home</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
         <!--/Fonts -->
 
+        <!-- CSS -->
         <link rel="stylesheet" href={{asset("css/app.css")}}>
+
+        <!--FAVICON-->
+        <link rel="apple-touch-icon" sizes="57x57" href={{asset("img/favicon/apple-icon-57x57.png")}}>
+        <link rel="apple-touch-icon" sizes="60x60" href={{asset("img/favicon/apple-icon-60x60.png")}}>
+        <link rel="apple-touch-icon" sizes="72x72" href={{asset("img/favicon/apple-icon-72x72.png")}}>
+        <link rel="apple-touch-icon" sizes="76x76" href={{asset("img/favicon/apple-icon-76x76.png")}}>
+        <link rel="apple-touch-icon" sizes="114x114" href={{asset("img/favicon/apple-icon-114x114.png")}}>
+        <link rel="apple-touch-icon" sizes="120x120" href={{asset("img/favicon/apple-icon-120x120.png")}}>
+        <link rel="apple-touch-icon" sizes="144x144" href={{asset("img/favicon/apple-icon-144x144.png")}}>
+        <link rel="apple-touch-icon" sizes="152x152" href={{asset("img/favicon/apple-icon-152x152.png")}}>
+        <link rel="apple-touch-icon" sizes="180x180" href={{asset("img/favicon/apple-icon-180x180.png")}}>
+        <link rel="icon" type="image/png" sizes="192x192"  href={{asset("img/favicon/android-icon-192x192.png")}}>
+        <link rel="icon" type="image/png" sizes="32x32" href={{asset("img/favicon/favicon-32x32.png")}}>
+        <link rel="icon" type="image/png" sizes="96x96" href={{asset("img/favicon/favicon-96x96.png")}}>
+        <link rel="icon" type="image/png" sizes="16x16" href={{asset("img/favicon/favicon-16x16.png")}}>
+        <link rel="icon" type="image/png" sizes="16x16" href={{asset("img/favicon/favicon.ico")}}>
 
     </head>
 
     <body>
         
+        <!--includo il file parts header-->
         @include('parts/header')
 
         <main>
@@ -52,6 +70,9 @@
                     </div>
                 </div>
         </main>
+
+        <!--includo il file parts footer-->
+        @include('parts/footer')
 
     </body>
 </html>
---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,56 @@
+
+<footer>
+    <div class="container">
+        <div class="footer-col-left">
+            <div class="footer-img">
+                <img src={{asset("img/marchio-sito-test.png")}} alt="logo del footer">
+            </div>
+            <ul>
+                <li><a>Ragione sociale: La Molisana S.P.A.</a></li>
+                <li><a>Sede legale: Contrada Colle delle Api, 100/A</a></li>
+                <li><a>86100 - Campobasso (CB)</a></li>
+                <li><a>Pec: lamolisana@pec.it</a></li>
+                <li><a>Tel: +39 0874 4981</a></li>
+                <li><a>Fax: +39 0874 629584</a></li>
+                <li><a>info@lamolisana.it (per segnalazioni degli utenti)</a></li>
+                <li><a>commerciale@lamolisana.it</a></li>
+                <li><a>numero verde 800818081</a></li>
+                <li><a>telefono 3801292455</a></li>
+            </ul>
+    
+        </div>
+        <div class="footer-col-center">
+            <h3>PASTIFICIO</h3>
+            <ul>
+                <li><a href="">Il Pastificio</a></li>
+                <li><a href="">Grano decorticato a pietra</a></li>
+                <li><a href="">Il Molise c'è!</a></li>
+                <li><a href="">Filiera Integrata</a></li>
+                <li><a href="">100 anni di pasta</a></li>
+                <li><a href="">Sartoria della pasta</a></li>
+                <li><a href="">Spaghetto Quadrato</a></li>
+                <li><a href="">Le Persone</a></li>
+            </ul>
+            <h3>PRODOTTI</h3>
+            <ul>
+                <li><a href="">Le Classiche</a></li>
+                <li><a href="">Le Integrali</a></li>
+                <li><a href="">Le Speciali</a></li>
+                <li><a href="">Le biologiche</a></li>
+                <li><a href="">Le Gluten-Free</a></li>
+                <li><a href="">Le Semole</a></li>
+                <li><a href="">Le Extra di Lusso</a></li>
+            </ul>
+
+        </div>
+        <div class="footer-col-right">
+            <h3>COLLEZIONI DA CHEF</h3>
+            <ul>
+                <li><a href="">Collezione da Chef</a></li>
+                <li><a href="">Grandi Cucine</a></li>
+                <li><a href="">Biologiche</a></li>
+                <li><a href="">Quadrate</a></li>
+            </ul>
+
+        </div>
+</footer>
